67dd1d467ca4ca5735e9fe35366d1ee15432e222
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-02-09 19:01:36 -0800

    Added completion conditions to QuestInformation

--- Assets/Scripts/Quest/QuestInformation.cs
+++ Assets/Scripts/Quest/QuestInformation.cs
@@ -6,39 +6,56 @@ using UnityEngine;
 
 public class QuestInformation : ScriptableObject
 {
+	[Header("Quest Description")]
+		[Tooltip("The name of this quest.")]
+		[SerializeField] private string m_questName;
+		public string questName
+		{
+			get {return m_questName;}
+			private set {return;}
+		}
 
-	[Tooltip("Number of steps required to complete this quest.")]
-	[SerializeField] private int m_maxProgress;
-	public int maxProgress
-	{
-		get {return m_maxProgress;}
-		private set{return;}
-	}
+		[Tooltip("The description of this quest.")]
+		[SerializeField] private string m_description;
+		public string description
+		{
+			get {return m_description;}
+			private set {return;}
+		}
 
-	[Tooltip("The description of this quest.")]
-	[SerializeField] private string m_description;
-	public string description
-	{
-		get {return m_description;}
-		private set {return;}
-	}
+	[Header("Quest Conditions")]
+		[Tooltip("Type of condition must be fulfilled to finish this quest.")]
+		[SerializeField] private QuestType m_type;
+		public QuestType type
+		{
+			get {return m_type;}
+			private set {return;}
+		}
 
-	[Tooltip("The name of this quest.")]
-	[SerializeField] private string m_name;
-	new public string name
-	{
-		get {return m_name;}
-		private set {return;}
-	}
+		[Tooltip("Condition that must be fulfilled to finish this quest.")]
+		[SerializeField] private string m_condition;
+		public string condition
+		{
+			get {return m_condition;}
+			private set {return;}
+		}
 
-	#nullable enable
-	[Tooltip("Next quest in this quest chain, if there is one.")]
-	[SerializeField] private QuestInformation? m_nextQuest;
-	public QuestInformation? nextQuest
-	{
-		get {return m_nextQuest;}
-		private set {return;}
-	}
-	#nullable disable
+		[Tooltip("Number of steps or amount of the condition required to complete this quest.")]
+		[SerializeField] private int m_maxProgress;
+		public int maxProgress
+		{
+			get {return m_maxProgress;}
+			private set {return;}
+		}
+
+		#nullable enable
+		[Tooltip("Next quest in this quest chain, if there is one.")]
+		[SerializeField] private QuestInformation? m_nextQuest;
+		public QuestInformation? nextQuest
+		{
+			get {return m_nextQuest;}
+			private set {return;}
+		}
+		#nullable disable
 
 }
---
+++ Assets/Scripts/Quest/QuestType.cs
@@ -0,0 +1,6 @@
+public enum QuestType
+{
+	Inventory,
+	Dialogue,
+	Other
+}
