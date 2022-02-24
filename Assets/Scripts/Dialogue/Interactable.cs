d7d851c174c9fdaadcd6a466752b047bc12d9e0b
DaphneVO <97932478+DaphneVO@users.noreply.github.com>
2022-02-24 00:01:32 -0800

    Updated Placeholders, NPC1 is interactable, dialogue runs, IntroScene updated with all current changes

--- Assets/Scripts/Dialogue/Interactable.cs
+++ Assets/Scripts/Dialogue/Interactable.cs
@@ -1,22 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Start is called before the first frame update The Start 
+    //function will be called by Unity before gameplay begins 
+    //(ie, before the Update function is called for the first time) 
+    //and is an ideal place to do any initialization.
+    private DialogueRunner dialogueRunner;
+    //public Dialogue IntroDialogue;
+    private bool interactable = true;
+    private bool isCurrentConversation = false;
+    private float defaultIndicatorIntensity;
+    public YarnProject IntroSceneProj;
+    //public string startNode = Yarn.Dialogue.DefaultStartNodeName;
+    public string startNode = "Start";
+    //public Camera MainCam;
+    //public Camera CamCHOM;
+
+     //Define List of Cameras
+    //private List<Camera> cameras;
+    
+    public void Start() 
     {
+       // cameras.Add(MainCam);
+       // cameras.Add(CamCHOM);
+        //SwapCamera(MainCam);
+        //dialogueRunner = FindObjectOfType<DialogueRunner>();
+        //dialogueRunner = GameObject.FindObjectOfType<DialogueRunner>();
+        //dialogueRunner.onDialogueComplete.AddListener(EndConversation);
         
     }
 
-    // Update is called once per frame The Update function 
-    //is the place to put code that will handle the frame 
-    //update for the GameObject. This might include movement, 
-    //triggering actions and responding to user input, basically 
-    //anything that needs to be handled over time during gameplay.
-    void Update()
-    {
-        
+
+    public void OnMouseDown() {
+        if (interactable)
+        {
+        //if (interactable && !dialogueRunner.IsDialogueRunning) {
+            StartConversation();
+        }
     }
+
+    private void StartConversation() {
+        Debug.Log($"Started conversation with {name}.");
+        //isCurrentConversation = true;
+        //SwapCamera(CamCHOM);
+        dialogueRunner = GameObject.FindObjectOfType<DialogueRunner>();
+        //dialogueRunner.StartDialogue(someStringVariable);
+        dialogueRunner.StartDialogue(startNode);
+    }
+
+   /*  public void SwapCamera(Camera cam){
+            // performance tradeoff
+            foreach(Camera c in cameras){
+                    c.enabled = false;
+            }
+            cam.enabled = true;
+    } */
+
+    private void EndConversation() {
+        if (isCurrentConversation) {
+            isCurrentConversation = false;
+            Debug.Log($"Ended conversation with {name}.");
+        }
+    }
+
+//    [YarnCommand("disable")]
+    public void DisableConversation() 
+    {
+        interactable = false;
+    }
+
 }
