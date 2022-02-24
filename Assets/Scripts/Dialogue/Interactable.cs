062d88ae659971d5aba143a3a87478ebf184b0f6
DaphneVO <97932478+DaphneVO@users.noreply.github.com>
2022-02-23 17:21:11 -0800

    Created Interactable.cs

---
+++ Assets/Scripts/Dialogue/Interactable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame The Update function 
+    //is the place to put code that will handle the frame 
+    //update for the GameObject. This might include movement, 
+    //triggering actions and responding to user input, basically 
+    //anything that needs to be handled over time during gameplay.
+    void Update()
+    {
+        
+    }
+}
