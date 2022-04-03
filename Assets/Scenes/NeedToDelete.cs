d0b81d72e5f9fbc9b65498938748099a934afc3f
Victor Fang <victorf588@gmail.com>
2022-04-02 17:43:33 -0700

    Add Settings
    Allow people to adjust the volume

---
+++ Assets/Scenes/NeedToDelete.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+    
+}
