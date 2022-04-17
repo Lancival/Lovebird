8168c292143ee548ee471b7f9e005ea607101a75
DaphneVO <97932478+DaphneVO@users.noreply.github.com>
2022-04-16 17:44:19 -0700

    test dialogue scene yarn command <<addQuest ex>>

--- Assets/Scripts/Dialogue/Test/DialogueTestFiles/Dialogue.cs
+++ Assets/Scripts/Dialogue/Test/DialogueTestFiles/Dialogue.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
 
 [System.Serializable]
-public class Dialogue : ScriptableObject
+public class Dialogue : MonoBehaviour
 {
     public string NPCname;
     
     public string[] sentences;
+    [YarnCommand("addQuest")]
+    public static void addQuest()
+    {
+        Debug.Log("Adding quest...");
+    }
 
 }
