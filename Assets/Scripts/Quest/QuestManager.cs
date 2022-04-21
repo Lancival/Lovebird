db376b1e8366232257eb31913c4385a161d9150d
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-04-20 20:17:24 -0700

    QuestManager.addQuest(string) implemented, fixed stack overflow bug with quest adding loop

--- Assets/Scripts/Quest/QuestManager.cs
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -1,8 +1,11 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public static class QuestManager
 {
+    private static Dictionary<string, Quest> allQuests;
+
     private static List<Quest> _quests = new List<Quest>();
     public static List<Quest> quests
     {
@@ -10,35 +13,55 @@ public static class QuestManager
         private set => _quests = value;
     }
 
+    private static readonly int questStackLimit = 10;
+    private static int questStack = 0;
+
+    [RuntimeInitializeOnLoadMethod]
+    private static void InitializeAllQuestDictionary()
+    {
+        allQuests = new Dictionary<string, Quest>();
+        foreach (Quest quest in Resources.LoadAll("Quests", typeof(Quest)))
+            allQuests.Add(quest.questName, quest);
+    }
+
     public static void Reset()
     {
-    	_quests = new List<Quest>();
+        _quests = new List<Quest>();
         UpdateQuestLog();
     }
 
     public static void Add(Quest quest)
     {
-    	if (quest == null)
-    	{
-    		return;
-    	}
+        if (quest == null)
+        {
+            return;
+        }
 
-    	_quests.Add(quest);
-    	if (quest.IsComplete())
-    	{
-    		quest.Finish();
-    	}
-    	else
-    	{
-    		quest.Subscribe();
-    	}
+        if (++questStack > questStackLimit)
+        {
+            --questStack;
+            return;
+        }
+
+        _quests.Add(quest);
+        if (quest.IsComplete())
+            quest.Finish();
+        else
+            quest.Subscribe();
 
         UpdateQuestLog();
+        --questStack;
+    }
+
+    public static void Add(string questName)
+    {
+        if (allQuests.ContainsKey(questName))
+            Add(allQuests[questName]);
     }
 
     public static void Remove(Quest quest)
     {
-    	_quests.Remove(quest);
+        _quests.Remove(quest);
         UpdateQuestLog();
     }
 
