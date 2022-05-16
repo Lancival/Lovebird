31d1cbe3935e2f95e1a2f4c8d2180cdab25b7376
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-05-15 17:57:52 -0700

    Implemented custom preview images for Item ScriptableObjects - Added ItemEditor class in Item.cs - Imported Fruits spritesheet - Created Warbleberry, Chowmelon, Sprinkfruit, Flux Heart, and Bous Lentil items

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "Quest", menuName = "ScriptableObjects/Items", order = 1)]
 public class Item : ScriptableObject
@@ -22,4 +23,31 @@ public class Item : ScriptableObject
     	public Sprite sprite => _sprite;
 
     public bool HasTag(string tag) => tags.Contains(tag);
+}
+
+[CustomEditor(typeof(Item))]
+public class ItemEditor : Editor
+{
+    public override Texture2D RenderStaticPreview(string assetPath, Object[] subAssets, int width, int height)
+    {
+        // Note: In the import settings for the sprite, the read/write checkbox must be checked!
+        Sprite sprite = ((Item) serializedObject.targetObject).sprite;
+        if (sprite != null)
+        {
+            Texture2D croppedTexture = new Texture2D((int) sprite.rect.width, (int) sprite.rect.height);
+            Color[] pixels = sprite.texture.GetPixels(
+                (int) sprite.rect.x,
+                (int) sprite.rect.y,
+                (int) sprite.rect.height,
+                (int) sprite.rect.width
+            );
+            croppedTexture.SetPixels(pixels);
+            croppedTexture.Apply();
+            return croppedTexture;
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
