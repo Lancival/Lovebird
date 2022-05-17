38a3e6a92757c91390b5ea9967d66fa04e0afa9a
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-05-17 14:09:23 -0700

    Only CalculateBounds once

--- Assets/Scripts/Map/CameraFollower.cs
+++ Assets/Scripts/Map/CameraFollower.cs
@@ -8,32 +8,31 @@ public class CameraFollower : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Transform player;
 
-    private float leftBound;
-    private float rightBound;
-    private float upperBound;
-    private float lowerBound;
-
-    void Awake() => cam = GetComponent<Camera>();
+    private float leftBound, rightBound, upperBound, lowerBound;
 
     private void CalculateBounds()
     {
         float verticalExtent = cam.orthographicSize;
         float horizontalExtent = verticalExtent * Screen.width / Screen.height;
-
         leftBound = horizontalExtent - spriteRenderer.sprite.bounds.size.x / 2.0f;
         rightBound = spriteRenderer.sprite.bounds.size.x / 2.0f - horizontalExtent;
-        lowerBound = verticalExtent - spriteRenderer.sprite.bounds.size.y / 2.0f;
+        lowerBound = cam.orthographicSize - spriteRenderer.sprite.bounds.size.y / 2.0f;
         upperBound = spriteRenderer.sprite.bounds.size.y / 2.0f - verticalExtent;
     }
 
-    void Start() => CalculateBounds();
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        CalculateBounds();
+    }
 
     void Update()
     {
-        CalculateBounds();
-        Vector3 target = new Vector3(player.position.x, player.position.y,transform.position.z);
-        target.x = Mathf.Clamp(target.x, leftBound, rightBound);
-        target.y = Mathf.Clamp(target.y, lowerBound, upperBound);
+        Vector3 target = new Vector3(
+            Mathf.Clamp(player.position.x, leftBound, rightBound),
+            Mathf.Clamp(player.position.y, lowerBound, upperBound),
+            transform.position.z
+        );
         transform.position = target;
     }
 }
