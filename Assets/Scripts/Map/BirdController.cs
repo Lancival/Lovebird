399332791073bb6c67e5bd6b372d7665ec410421
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-05-19 19:37:55 -0700

    Map animations - Reimplemented cloud floating animation - Implemented Lume bird form animations

--- Assets/Scripts/Map/BirdController.cs
+++ Assets/Scripts/Map/BirdController.cs
@@ -3,16 +3,41 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 
 public class BirdController : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    // Components
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Movement Variables
     private Vector2 movement = new Vector2();
     [SerializeField] private float speed = 5.0f;
 
-    void Awake() => rb = GetComponent<Rigidbody2D>();
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
 
-    public void OnMove(InputAction.CallbackContext context) => movement = context.ReadValue<Vector2>();
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        animator.SetBool("isMoving", movement.magnitude != 0);
 
-    void FixedUpdate() => rb.velocity = movement * speed;
+        if (movement.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movement.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    void FixedUpdate() => rigidBody.velocity = movement * speed;
 }
