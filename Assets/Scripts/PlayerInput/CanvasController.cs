08f331a610661eeccf60894a0afa21a0b23d5367
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-05-25 16:00:34 -0700

    CanvasController duration

--- Assets/Scripts/PlayerInput/CanvasController.cs
+++ Assets/Scripts/PlayerInput/CanvasController.cs
@@ -5,15 +5,18 @@ public class CanvasController : MonoBehaviour
     private static CanvasController _instance;
     public static CanvasController instance => _instance;
 
-    [SerializeField] private FadeCanvasGroup inventory;
-    [SerializeField] private FadeCanvasGroup questLog;
-    [SerializeField] private FadeCanvasGroup settings;
+    [SerializeField] private float duration = 0.5f;
+
+    [Header("Canvases")]
+        [SerializeField] private FadeCanvasGroup inventory;
+        [SerializeField] private FadeCanvasGroup questLog;
+        [SerializeField] private FadeCanvasGroup settings;
 
     void Awake() => _instance = this;
     void OnDestroy() => _instance = null;
 
-    public static void OpenInventory() => _instance?.inventory?.FadeIn();
-    public static void OpenQuestLog() => _instance?.questLog?.FadeIn();
-    public static void OpenSettings() => _instance?.settings?.FadeIn();
+    public static void OpenInventory() => _instance?.inventory?.FadeIn(_instance.duration);
+    public static void OpenQuestLog() => _instance?.questLog?.FadeIn(_instance.duration);
+    public static void OpenSettings() => _instance?.settings?.FadeIn(_instance.duration);
     public static void OpenMap() => SceneLoader.instance?.LoadScene("Map");
 }
