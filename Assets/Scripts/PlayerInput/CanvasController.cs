b07ebcf7415ba262add86766fda51aa59e45e28d
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-05-25 20:46:45 -0700

    Chom Chom

--- Assets/Scripts/PlayerInput/CanvasController.cs
+++ Assets/Scripts/PlayerInput/CanvasController.cs
@@ -7,6 +7,7 @@ public class CanvasController : MonoBehaviour
     public static CanvasController instance => _instance;
 
     [Header("Canvases")]
+        [SerializeField] private FadeCanvasGroup trade;
         [SerializeField] private Button inventory;
         [SerializeField] private Button questLog;
         [SerializeField] private Button map;
@@ -16,6 +17,13 @@ public class CanvasController : MonoBehaviour
     void Awake() => _instance = this;
     void OnDestroy() => _instance = null;
 
+    public static void OpenTrade()
+    {
+        _instance?.trade?.FadeIn();
+        CanvasGroup canvasGroup = _instance?.trade?.gameObject.GetComponent<CanvasGroup>();
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
     public static void OpenInventory() => _instance?.inventory?.onClick?.Invoke();
     public static void OpenQuestLog() => _instance?.questLog?.onClick?.Invoke();
     public static void OpenSettings() => _instance?.settings?.onClick?.Invoke();
